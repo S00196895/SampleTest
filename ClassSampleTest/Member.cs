af8f8e4fbccba5cf69ad16eb9eb6cb1ad20ad395
Bartlomiej Sajdok <S00196895@mail.itsligo.ie>
2022-01-08 13:47:29 +0000

    Add project files.

---
+++ ClassSampleTest/Member.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSampleTest
+{
+    class Member
+    {
+        [Key]
+        [Display(Name = "Member ID")]
+        public string MemberID { get; set; }
+
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+
+        [Column(TypeName = "datetime")]
+        [DataType(DataType.Date)]
+        public DateTime DateJoined { get; set; }
+    }
+}
